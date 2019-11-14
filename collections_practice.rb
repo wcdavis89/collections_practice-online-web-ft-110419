0d3ebac1672f6fad254803c8e0fa7f7db214eac2
Whittney Davis <wcdavis89@yahoo.com>
2019-11-14 02:27:05 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,7 +7,13 @@ def sort_array_desc(array)
   return new_array.reverse
 end
 
-def sort_array_char_count(array)
-    array.sort do
+  def sort_array_char_count(array)
+  array.sort do |a,b|
+  if a.size<b.size
+  -1
+  else a.size>b.size
+  1
   end
+  end
+  array.sort
 end
